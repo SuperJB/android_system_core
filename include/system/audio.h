ac164ab86d747e6ab462db5c621f9995202f6476
podxboq <podxboq@gmail.com>
2012-12-10 13:30:06 +0100

    Error: AUDIO_DEVICE_IN_REMOTE_SUBMIX was not declared in this scope

--- include/system/audio.h
+++ include/system/audio.h
@@ -361,12 +361,12 @@ typedef enum {
     AUDIO_DEVICE_OUT_USB_ACCESSORY             = 0x2000,
     AUDIO_DEVICE_OUT_USB_DEVICE                = 0x4000,
     AUDIO_DEVICE_OUT_REMOTE_SUBMIX             = 0x8000,
-#ifdef QCOM_FM_ENABLED
-    AUDIO_DEVICE_OUT_FM                        = 0x10000,
-    AUDIO_DEVICE_OUT_FM_TX                     = 0x20000,
-#endif
 #ifdef QCOM_HARDWARE
-    AUDIO_DEVICE_OUT_PROXY                     = 0x80000,
+    AUDIO_DEVICE_OUT_PROXY                     = 0x10000,
+#ifdef QCOM_FM_ENABLED
+    AUDIO_DEVICE_OUT_FM                        = 0x20000,
+    AUDIO_DEVICE_OUT_FM_TX                     = 0x40000,
+#endif
 #endif
     AUDIO_DEVICE_OUT_DEFAULT                   = AUDIO_DEVICE_BIT_DEFAULT,
     AUDIO_DEVICE_OUT_ALL      = (AUDIO_DEVICE_OUT_EARPIECE |
@@ -384,12 +384,12 @@ typedef enum {
                                  AUDIO_DEVICE_OUT_DGTL_DOCK_HEADSET |
                                  AUDIO_DEVICE_OUT_USB_ACCESSORY |
                                  AUDIO_DEVICE_OUT_USB_DEVICE |
+#ifdef QCOM_HARDWARE
+                                 AUDIO_DEVICE_OUT_PROXY |
 #ifdef QCOM_FM_ENABLED
                                  AUDIO_DEVICE_OUT_FM |
                                  AUDIO_DEVICE_OUT_FM_TX |
 #endif
-#ifdef QCOM_HARDWARE
-                                 AUDIO_DEVICE_OUT_PROXY |
 #endif
                                  AUDIO_DEVICE_OUT_REMOTE_SUBMIX |
                                  AUDIO_DEVICE_OUT_DEFAULT),
@@ -403,36 +403,6 @@ typedef enum {
                                  AUDIO_DEVICE_OUT_USB_DEVICE),
 
     /* input devices */
-#ifdef QCOM_HARDWARE
-    AUDIO_DEVICE_IN_COMMUNICATION         = 0x100000,
-    AUDIO_DEVICE_IN_AMBIENT               = 0x200000,
-    AUDIO_DEVICE_IN_BUILTIN_MIC           = 0x400000,
-    AUDIO_DEVICE_IN_BLUETOOTH_SCO_HEADSET = 0x800000,
-    AUDIO_DEVICE_IN_WIRED_HEADSET         = 0x1000000,
-    AUDIO_DEVICE_IN_AUX_DIGITAL           = 0x2000000,
-    AUDIO_DEVICE_IN_VOICE_CALL            = 0x4000000,
-    AUDIO_DEVICE_IN_BACK_MIC              = 0x8000000,
-#ifdef QCOM_FM_ENABLED
-    AUDIO_DEVICE_IN_FM_RX                 = 0x20000000,
-    AUDIO_DEVICE_IN_FM_RX_A2DP            = 0x40000000,
-#endif
-    AUDIO_DEVICE_IN_PROXY                 = 0x80000000,
-    AUDIO_DEVICE_IN_DEFAULT               = AUDIO_DEVICE_IN_BUILTIN_MIC,
-#else
-    AUDIO_DEVICE_IN_COMMUNICATION         = 0x10000,
-    AUDIO_DEVICE_IN_AMBIENT               = 0x20000,
-    AUDIO_DEVICE_IN_BUILTIN_MIC           = 0x40000,
-    AUDIO_DEVICE_IN_BLUETOOTH_SCO_HEADSET = 0x80000,
-    AUDIO_DEVICE_IN_WIRED_HEADSET         = 0x100000,
-    AUDIO_DEVICE_IN_AUX_DIGITAL           = 0x200000,
-    AUDIO_DEVICE_IN_VOICE_CALL            = 0x400000,
-    AUDIO_DEVICE_IN_BACK_MIC              = 0x800000,
-#ifdef ALLWINNER
-    AUDIO_DEVICE_IN_AF                    = 0x1000000,
-#endif
-    AUDIO_DEVICE_IN_DEFAULT               = 0x80000000,
-#endif
-/* --adlx this comes from aosp/master
     AUDIO_DEVICE_IN_COMMUNICATION         = AUDIO_DEVICE_BIT_IN | 0x1,
     AUDIO_DEVICE_IN_AMBIENT               = AUDIO_DEVICE_BIT_IN | 0x2,
     AUDIO_DEVICE_IN_BUILTIN_MIC           = AUDIO_DEVICE_BIT_IN | 0x4,
@@ -447,7 +417,16 @@ typedef enum {
     AUDIO_DEVICE_IN_USB_ACCESSORY         = AUDIO_DEVICE_BIT_IN | 0x800,
     AUDIO_DEVICE_IN_USB_DEVICE            = AUDIO_DEVICE_BIT_IN | 0x1000,
     AUDIO_DEVICE_IN_DEFAULT               = AUDIO_DEVICE_BIT_IN | AUDIO_DEVICE_BIT_DEFAULT,
-*/
+#ifdef ALLWINNER
+    AUDIO_DEVICE_IN_AF                    = AUDIO_DEVICE_BIT_IN | 0x2000,
+#endif
+#ifdef QCOM_HARDWARE
+    AUDIO_DEVICE_IN_PROXY                 = AUDIO_DEVICE_BIT_IN | 0x2000,
+#ifdef QCOM_FM_ENABLED
+    AUDIO_DEVICE_IN_FM_RX                 = AUDIO_DEVICE_BIT_IN | 0x4000,
+    AUDIO_DEVICE_IN_FM_RX_A2DP            = AUDIO_DEVICE_BIT_IN | 0x8000,
+#endif
+#endif
     AUDIO_DEVICE_IN_ALL     = (AUDIO_DEVICE_IN_COMMUNICATION |
                                AUDIO_DEVICE_IN_AMBIENT |
                                AUDIO_DEVICE_IN_BUILTIN_MIC |
@@ -459,11 +438,11 @@ typedef enum {
 #ifdef ALLWINNER
 			       AUDIO_DEVICE_IN_AF |
 #endif
+#ifdef QCOM_HARDWARE
 #ifdef QCOM_FM_ENABLED
                                AUDIO_DEVICE_IN_FM_RX |
                                AUDIO_DEVICE_IN_FM_RX_A2DP |
 #endif
-#ifdef QCOM_HARDWARE
                                AUDIO_DEVICE_IN_PROXY |
 #endif
                                AUDIO_DEVICE_IN_REMOTE_SUBMIX |
